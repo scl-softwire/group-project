5140c67909d4966dde31346798fd585d857c8f6c
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-05-01 16:48:47 +0100

    Added Delete functionality

--- src/main/java/org/softwire/training/slideshowbob/controllers/ImageController.java
+++ src/main/java/org/softwire/training/slideshowbob/controllers/ImageController.java
@@ -5,6 +5,7 @@ import org.softwire.training.slideshowbob.services.ImagesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -23,5 +24,13 @@ public class ImageController {
 
         // TODO: add the real redirect URL
         return new RedirectView("TODO");
+    }
+
+    @RequestMapping("/image-delete/{id}")
+    RedirectView deleteImage(@PathVariable("id") Integer imageId) {
+
+        imagesService.deleteImage(imageId);
+
+        return new RedirectView("/images");
     }
 }
