0e5ad954be4509d25ebde1ffde9d58f3ed3af08c
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-05-01 16:27:08 +0100

    Added getAllImages script

--- src/main/java/org/softwire/training/slideshowbob/services/ImagesService.java
+++ src/main/java/org/softwire/training/slideshowbob/services/ImagesService.java
@@ -4,6 +4,7 @@ import org.softwire.training.slideshowbob.models.database.UploadImages;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ImagesService extends DatabaseService {
@@ -11,6 +12,14 @@ public class ImagesService extends DatabaseService {
         public void uploadImage (UploadImages image) {
             jdbi.useHandle(handle -> handle.createUpdate("INSERT INTO upload_images (date_time_stamp, image_name, author, license, url) " +
                     "VALUES (" + LocalDateTime.now() + ")"));
+        }
+
+        public List<UploadImages> getAllImages () {
+            return jdbi.withHandle(handle ->
+                    handle.createQuery("SELECT * FROM upload_images ")
+                    .mapToBean(UploadImages.class)
+                    .list()
+                    );
         }
 
 
