924388a5fe76ccecf0ac27cca4d208bb58e3a8c1
Joel Rimmer <joel.rimmer@valtech.com>
2019-05-02 13:31:15 +0100

    edit

--- src/main/java/org/softwire/training/slideshowbob/controllers/AdminController.java
+++ src/main/java/org/softwire/training/slideshowbob/controllers/AdminController.java
@@ -9,11 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping ("/admin")
+@RequestMapping("/admin")
 public class AdminController {
 
     private final ImagesService imagesService;
@@ -25,25 +26,34 @@ public class AdminController {
         this.authService = authService;
     }
 
-    // TODO - add service
-
-
     @RequestMapping("")
     ModelAndView admin() {
         return new ModelAndView("admin");
     }
 
-
-
     @RequestMapping("/select-images")
     ModelAndView allImages() {
         List<Image> allImages = new ArrayList<>();
         return new ModelAndView("select-images", "model", new ImagePageModel(allImages));
+    }
 
     @RequestMapping("/uploadImage")
     ModelAndView uploadImage() {
         return new ModelAndView("uploadImage");
+    }
 
+    @RequestMapping("/manage/edit") ModelAndView edit() {
+        // TODO: Get image from DB
+
+        Image image = new Image();
+        image.setId(1);
+        image.setAuthor("Joel");
+        image.setImageName("G");
+        image.setLicense("Asdf");
+        image.setDateTimeStamp(LocalDateTime.now());
+        image.setUrl("https://hips.hearstapps.com/esquireuk.cdnds.net/17/08/1024x576/hd-aspect-1487849133-c2d79717-d113-442f-9165-af9685a4e404.jpg?resize=480:*");
+
+        return new ModelAndView("edit", "image", image);
     }
 
 }
